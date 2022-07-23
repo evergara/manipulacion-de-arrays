ad10ddde681370aa4b86c18b3f36e84bf7523083
Emerson Vergara <emerson.vergara@ceiba.com.co>
2022-07-22 20:39:37 -0500

    menejo de for inicial

---
+++ src/server/01_for-each/01-for-each.js
@@ -0,0 +1,13 @@
+const letters = ['a', 'b', 'C', 'd'];
+const animales
+
+for (let index = 0; index < letters.length; index++) {
+    const element = Letters[index];
+    console.log('FOR', letters.toUpperCase());
+}
+
+letters.forEach(item => {
+    console.log('forEach',item.toUpperCase());
+});
+
+funtion()
