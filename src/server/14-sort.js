54c620106b577e356b4de8896a3788d1ec46ce30
Emerson Vergara <ee-web@hotmail.com>
2022-11-28 20:13:23 -0500

    Metodos mutables sort

---
+++ src/server/14-sort.js
@@ -0,0 +1,73 @@
+const months2 = ["March", "Jan", "Feb", "Dec"];
+
+const numbers = [1, 30, 4, 21, 100000];
+
+const words = [
+    "réservé",
+    "premier",
+    "communiqué",
+    "café",
+    "adieu",
+    "éclair",
+    "banana",
+];
+
+const orders = [
+    {
+        customerName: "Nicolas",
+        total: 600,
+        delivered: true,
+    },
+    {
+        customerName: "Zulema",
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: "Emerson",
+        total: 1840,
+        delivered: true,
+    },
+    {
+        customerName: "Valentina",
+        total: 240,
+        delivered: true,
+    },
+];
+
+const months = ['Febrero', 'Julio', "fsdf", 'Diciembre', 'Enero'];
+function monthValue(month) {
+    switch (month.toUpperCase()) {
+        case 'ENERO':
+            return 1;
+        case 'FEBRERO':
+            return 2;
+        case 'MARZO':
+            return 3;
+        case 'ABRIL':
+            return 4;
+        case 'MAYO':
+            return 5;
+        case 'JUNIO':
+            return 6;
+        case 'JULIO':
+            return 7;
+        case 'AGOSTO':
+            return 8;
+        case 'SEPTIEMBRE':
+            return 9;
+        case 'OCTUBRE':
+            return 10;
+        case 'NOVIEMBRE':
+            return 11;
+        case 'DICIEMBRE':
+            return 12;
+        default:
+            //Cualquier valor que no coincida se irá de último
+            return 13; 
+    }
+}
+
+months.sort((a, b) => monthValue(a) - monthValue(b))
+
+console.log(months);
